7080fb6c53929e319b7440077d9a4afa3df340e9
karin morais <andradkarin@gmail.com>
2023-08-17 14:25:26 -0300

    Added function to change pages

--- linkedin-script.js
+++ linkedin-script.js
@@ -1,13 +1,13 @@
 async function AddConnections() {
-    let results = await getResults();
+	let results = await getResults();
 
-    for (const person of results) {
-        await addPerson(person);
-    }
+	for (const person of results) {
+		await addPerson(person);
+	}
 }
 
 async function addPerson(person) {
-    return new Promise(resolve => {
+	return new Promise(resolve => {
 		clickButton(person).then(async () => {
 			setTimeout(async () => {
 				let addNoteButton = document.querySelector(".artdeco-modal__actionbar button[aria-label='Add a note']");
@@ -15,40 +15,48 @@ async function addPerson(person) {
 				await sendMessage(addNoteButton);
 
 				let sendButton = document.querySelector(".artdeco-modal__actionbar.ember-view button[aria-label='Send now']");
-                setTimeout(async () => {
-                    await clickButton(sendButton);
-                }, 600);
+				setTimeout(async () => {
+					await clickButton(sendButton);
+				}, 600);
 				resolve();
 			}, 600);
 		});
-    });
+	});
 }
 
 async function sendMessage(button) {
-    return new Promise(resolve => {
+	return new Promise(resolve => {
 		button.click();
-        setTimeout(() => {
-            let el = document.querySelector("textarea[name='message']")
-            el.value = " ";
-            let evt = document.createEvent("Events");
-            evt.initEvent("change", true, true);
-            el.dispatchEvent(evt);
-        }, 200);
-        resolve();
-    });
+		setTimeout(() => {
+			let el = document.querySelector("textarea[name='message']")
+			el.value = " ";
+			let evt = document.createEvent("Events");
+			evt.initEvent("change", true, true);
+			el.dispatchEvent(evt);
+		}, 200);
+		resolve();
+	});
 }
 
 async function clickButton(button) {
-    return new Promise(resolve => {
-        button.click();
-        resolve();
-    });
+	return new Promise(resolve => {
+		button.click();
+		resolve();
+	});
 }
 
 async function getResults() {
-    return [...document.querySelectorAll(".entity-result .artdeco-button")].filter(a => {
-        return a.textContent.trim() === "Connect";
-    }).splice(0, 1);
+	return [...document.querySelectorAll(".entity-result .artdeco-button")].filter(a => {
+		return a.textContent.trim() === "Connect";
+	}).splice(0, 1);
 }
 
-AddConnections();
+
+function changePage() {
+	let pagination = document.querySelector(".artdeco-pagination");
+	let currentPage = pagination.querySelector(".active");
+	console.log(currentPage);
+}
+
+// AddConnections();
+changePage()
